f666504b8d502b2497af700fa92ffb3a7cf5008d
mayur <mayurpatil5703@gmail.com>
2025-10-30 13:15:30 +0530

    Day 3

---
+++ src/Arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 1, 1, 1};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] arr) {
+        int count = 0, maxCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+}
