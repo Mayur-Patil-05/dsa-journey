ff0f9ba784af78c395e34e7b4c87f104cd3f90f3
mayur <mayurpatil5703@gmail.com>
2025-11-06 14:12:36 +0530

    Day 10

---
+++ src/BinarySearch/PeakIndexInMountainArray.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+public class PeakIndexInMountainArray {
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 1, 0};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
---
+++ src/BinarySearch/SearchInRotatedSortedArray.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println(search(arr, target));
+    }
+
+    public static int search(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) return mid;
+
+            if (arr[left] <= arr[mid]) {
+                if (arr[left] <= target && arr[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (arr[right] >= target && arr[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
