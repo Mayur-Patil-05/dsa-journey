b6adecf34e41c381be83d84d24d0dae9479e590d
mayur <mayurpatil5703@gmail.com>
2025-11-14 20:34:33 +0530

    Day 15

---
+++ src/Matrix/AdditionOfMatrices.java
@@ -0,0 +1,32 @@
+package Matrix;
+
+public class AdditionOfMatrices {
+    public static void main(String[] args) {
+        int rows = 2, columns = 2;
+
+        int[][] A = {
+                {1, 2},
+                {3, 4}
+        };
+
+        int[][] B = {
+                {5, 6},
+                {7, 8}
+        };
+
+        int[][] sum = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                sum[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
